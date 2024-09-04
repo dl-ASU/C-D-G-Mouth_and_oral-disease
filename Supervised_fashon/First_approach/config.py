ea7d16b402408e33a535b1a64ebef2d8d02544b7
WaleedAlzamil80 <WaleedAlzamil80@gmail.com>
2024-09-04 20:00:57 +0300

    dir

--- Supervised_fashon/First_approach/config.py
+++ Supervised_fashon/First_approach/config.py
@@ -18,5 +18,5 @@ imagenet_std=[0.229, 0.224, 0.225]
 
 test_size = 0.3
 
-full_train_data_path = "/kaggle/input/dpr-dataset/preprocessed_images_299"
-full_test_data_path = "/kaggle/input/dpr-dataset/test_preprocessed_images_299"
+full_train_data_path = "/kaggle/input/dpr-dataset/preprocessed_images_299/preprocessed_images_299"
+full_test_data_path = "/kaggle/input/dpr-dataset/test_preprocessed_images_299/test_preprocessed_images_299"
