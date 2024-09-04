2198016a9a07db4a68b3eccc62fa642518458c2e
Mohammad Dallash <105324962+MohammadDallash@users.noreply.github.com>
2024-09-04 06:09:17 +0300

    fix the data leakage issue

--- Supervised_fashon/inception net/code/Dataset.py
+++ Supervised_fashon/inception net/code/Dataset.py
@@ -7,40 +7,45 @@ import numpy as np
 import random
 
 class CustomImageDataset(Dataset):
-    def __init__(self, root_dir, transform=None, shuffle=True):
+    def __init__(self, image_paths = [], labels = [], root_dir = '', transform=None, shuffle=True):
         self.root_dir = root_dir
         self.transform = transform
-        self.image_paths = []
-        self.labels = []
+        self.image_paths = image_paths
+        self.labels = labels
         self.class_map = {}
 
+        if len(self.image_paths) == 0 or len(self.labels) == 0:
+            self.image_paths = []
+            self.labels = []
 
-        anatomical_locations = sorted(os.listdir(root_dir))
+            assert root_dir != ''
 
-        current_index = 0
-        for anatomical_location in anatomical_locations:
+            anatomical_locations = sorted(os.listdir(root_dir))
 
-            location_path = os.path.join(root_dir, anatomical_location)
+            current_index = 0
+            for anatomical_location in anatomical_locations:
 
-            if os.path.isdir(location_path) and not anatomical_location.startswith('.'):
+                location_path = os.path.join(root_dir, anatomical_location)
 
-                risk_levels =  sorted(os.listdir(location_path))
-                for risk_level in risk_levels:
+                if os.path.isdir(location_path) and not anatomical_location.startswith('.'):
 
-                    risk_path = os.path.join(location_path, risk_level)
-                    if os.path.isdir(risk_path) and not risk_level.startswith('.'):
-                        class_name = f"{anatomical_location}_{risk_level}"
-                        if class_name not in self.class_map:
-                            self.class_map[class_name] = current_index
-                            current_index += 1
+                    risk_levels =  sorted(os.listdir(location_path))
+                    for risk_level in risk_levels:
 
-                        for img_name in os.listdir(risk_path):
-                            img_path = os.path.join(risk_path, img_name)
-                            if img_path.endswith(('.jpg', '.jpeg', '.png', '.bmp', '.tiff', '.webp')):
-                                self.image_paths.append(img_path)
-                                self.labels.append(self.class_map[class_name])
+                        risk_path = os.path.join(location_path, risk_level)
+                        if os.path.isdir(risk_path) and not risk_level.startswith('.'):
+                            class_name = f"{anatomical_location}_{risk_level}"
+                            if class_name not in self.class_map:
+                                self.class_map[class_name] = current_index
+                                current_index += 1
 
-        
+                            for img_name in os.listdir(risk_path):
+                                img_path = os.path.join(risk_path, img_name)
+                                if img_path.endswith(('.jpg', '.jpeg', '.png', '.bmp', '.tiff', '.webp')):
+                                    self.image_paths.append(img_path)
+                                    self.labels.append(self.class_map[class_name])
+
+            
         
         if shuffle:
             self._shuffle()
