2eab9f42d146fe49bdb84328e1e0023951a27137
WaleedAlzamil80 <WaleedAlzamil80@gmail.com>
2024-09-05 12:48:10 +0300

    ss

--- Supervised_fashon/First_approach/config.py
+++ Supervised_fashon/First_approach/config.py
@@ -5,7 +5,7 @@ embedding_dim = 128
 learning_rate = 0.001
 num_epochs = 30
 l2 = 1e-6
-batch_size = 256
+batch_size = 128
 gamma = 0.75
 
 sche_milestones = [10, 20, 30, 40, 42, 43, 44, 45, 46, 47, 48, 49]
@@ -18,8 +18,8 @@ imagenet_std=[0.229, 0.224, 0.225]
 
 test_size = 0.3
 
-# full_train_data_path = "/home/waleed/Documents/Medical/data_DRP/preprocessed_images_299"
-# full_test_data_path = "/home/waleed/Documents/Medical/data_DRP/test_preprocessed_images_299"
+full_train_data_path = "/home/waleed/Documents/Medical/data_DRP/preprocessed_images_299"
+full_test_data_path = "/home/waleed/Documents/Medical/data_DRP/test_preprocessed_images_299"
 
-full_train_data_path = "/kaggle/input/dpr-dataset/preprocessed_images_299/preprocessed_images_299"
-full_test_data_path = "/kaggle/input/dpr-dataset/test_preprocessed_images_299/test_preprocessed_images_299"
+# full_train_data_path = "/kaggle/input/dpr-dataset/preprocessed_images_299/preprocessed_images_299"
+# full_test_data_path = "/kaggle/input/dpr-dataset/test_preprocessed_images_299/test_preprocessed_images_299"
