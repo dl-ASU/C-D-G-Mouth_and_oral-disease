acd764ecb284f1facaac3f57786f17a387c58b94
Yomnaa Muhammedd <yomnaamuhammedd@192.168.1.10>
2024-10-23 16:56:51 +0300

    updated dataset file

--- script_dyn_crop.py
+++ script_dyn_crop.py
@@ -1,83 +1,63 @@
 import cv2
-import numpy as np
 import os
+from helpful.read_imgs import read_image
 
+def dynamic_cropping(image_path, txt_path, crop_percentage=0.25):
 
-def dynamic_cropping(image_folder, txt_folder, output_folder, crop_percentage=0.5):
+        image, _ = read_image(image_path)
 
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+        if image is None:
+            print(f"Could not open {image_path.split('/')[0]}")
 
-    for image_filename in os.listdir(image_folder):
-        if image_filename.endswith(".jpg") or image_filename.endswith(".png"):
+        height, width, _ = image.shape
 
-            image_path = os.path.join(image_folder, image_filename)
-            image = cv2.imread(image_path)
+        # Load corresponding .txt file
 
-            if image is None:
-                print(f"Could not open {image_filename}")
-                continue
+        #check if the txt file exists or not aslan 
+        if not os.path.exists(txt_path):
+            print(f"Could not find corresponding txt file for {image_path.split('/')[0]}")
 
-            height, width, _ = image.shape
-            min_dim = min(height, width)
-            crop_size = int(crop_percentage * min_dim)
+        # Extract x_center, y_center from the .txt file
+        with open(txt_path, 'r') as file:
+            line = file.readline()
+            _, x_center, y_center, w, h = map(float, line.split())
 
-            # Load corresponding .txt file
-            txt_filename = image_filename.replace(".jpg", ".txt").replace(".png", ".txt")
-            txt_path = os.path.join(txt_folder, txt_filename)
+        x_center_pixel = int(x_center * width)
+        y_center_pixel = int(y_center * height)
+        w = int(w * width)
+        h = int(h * height)
 
-            #check if the txt file exists or not aslan 
-            if not os.path.exists(txt_path):
-                print(f"Could not find corresponding txt file for {image_filename}")
-                continue
+        min_dim = min(height, width)
+        crop_size = int(crop_percentage * min_dim)
+        crop_size = max(crop_size, w, h)
 
-            # Extract x_center, y_center from the .txt file
-            with open(txt_path, 'r') as file:
-                line = file.readline()
-                _, x_center, y_center, _, _ = map(float, line.split())
+        #will check later if the square is really square or not
+        x1 = x_center_pixel - crop_size // 2
+        y1 = y_center_pixel - crop_size // 2
+        x2 = x_center_pixel + crop_size // 2
+        y2 = y_center_pixel + crop_size // 2
 
-            x_center_pixel = int(x_center * width)
-            y_center_pixel = int(y_center * height)
+        #shift right if the left side goes out of bounds
+        if x1 < 0:
+            x2 += abs(x1)
+            x1 = 0
 
-            #will check later if the square is really square or not
-            x1 = x_center_pixel - crop_size // 2
-            y1 = y_center_pixel - crop_size // 2
-            x2 = x_center_pixel + crop_size // 2
-            y2 = y_center_pixel + crop_size // 2
+        #shift down if the top side goes out of bounds
+        if y1 < 0:
+            y2 += abs(y1)  
+            y1 = 0
 
-            #shift right if the left side goes out of bounds
-            if x1 < 0:
-                x2 += abs(x1)
-                x1 = 0
+        #shift left if the right side goes out of bounds
+        if x2 > width:
+            x1 -= (x2 - width)  
+            x2 = width
 
-            #shift down if the top side goes out of bounds
-            if y1 < 0:
-                y2 += abs(y1)  
-                y1 = 0
+        #shift up if the bottom side goes out of bounds
+        if y2 > height:
+            y1 -= (y2 - height)
+            y2 = height
 
-            #shift left if the right side goes out of bounds
-            if x2 > width:
-                x1 -= (x2 - width)  
-                x2 = width
+        # Crop the image
+        cropped_image = image[y1:y2, x1:x2]
 
-            #shift up if the bottom side goes out of bounds
-            if y2 > height:
-                y1 -= (y2 - height)
-                y2 = height
-
-            # Crop the image
-            cropped_image = image[y1:y2, x1:x2]
-
-            # Save the processed image
-            output_image_path = os.path.join(output_folder, image_filename)
-            cv2.imwrite(output_image_path, cropped_image)
-
-            print(f"Processed and saved {output_image_path}")
-
-if __name__ == "__main__":
-    image_folder = 'working/runs/detect/predict/detected original img'  # Folder with images
-    txt_folder = 'working/runs/detect/predict/detected one box txt'  # Folder with corresponding .txt files
-    output_folder = 'working/runs/detect/predict/cropped_images'  # Folder to save cropped and processed images
-
-    dynamic_cropping(image_folder, txt_folder, output_folder)
-
+        return cropped_image, image
